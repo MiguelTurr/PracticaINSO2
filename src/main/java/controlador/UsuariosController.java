4f61cb17d65d69b523ab851f3aee230cfd7f90a2
MiguelTurr <crest@192.168.1.129>
2022-05-28 03:16:07 +0200

    ver, borrar, editar y crear usuarios / arreglos

--- src/main/java/controlador/UsuariosController.java
+++ src/main/java/controlador/UsuariosController.java
@@ -2,18 +2,15 @@ package controlador;
 
 import modelo.Usuarios;
 import controlador.util.JsfUtil;
-import controlador.util.JsfUtil.PersistAction;
 import EJB.UsuariosFacade;
 import java.io.IOException;
 
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
-import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
-import javax.ejb.EJBException;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.component.UIComponent;
@@ -22,6 +19,7 @@ import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
+import modelo.Roles;
 
 @Named("usuariosController")
 @SessionScoped
@@ -30,122 +28,51 @@ public class UsuariosController implements Serializable {
     @EJB
     private UsuariosFacade ejbFacade;
     private List<Usuarios> items = null;
-    private Usuarios selected;
     
-    private Usuarios userInfo;
-    
-    private String clienteNombre;
-    private String clienteApellido1;
-    private String clienteApellido2;
-    private String clienteDni;
-    private String clienteTelefono;
-    private Date clienteFecha;
-    private String clienteUsuario;
-    private String clientePassword;
-    
+    private Usuarios usuarioInfo;
     private Usuarios usuarioCreado = null;
+    
+    public Usuarios nuevoUsuarioInfo(Roles rol) throws IOException {
+        this.usuarioInfo = new Usuarios();
+        this.usuarioInfo.setIdRol(rol);
+        
+        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+        ec.redirect("crearCliente.xhtml");
+        
+        return this.usuarioInfo;
+    }
+    
+    public Usuarios nuevoUsuarioInfo() {
+        this.usuarioInfo = new Usuarios();
+        return this.usuarioInfo;
+    }
+    
+    public Usuarios editarUsuarioInfo(Usuarios user) {
+        this.usuarioInfo = user;
+        return this.usuarioInfo;
+    }
+    
+    public void setUsuarioInfo(Usuarios user) {
+        this.usuarioInfo = user;
+    }
+    
+    public Usuarios getUsuarioInfo() {
+        return this.usuarioInfo;
+    }
 
     public UsuariosController() {
     }
-    
-    public String getClienteNombre() {
-        return this.clienteNombre;
-    }
-    public String getClienteApellido1() {
-        return this.clienteApellido1;
-    }
-    public String getClienteApellido2() {
-        return this.clienteApellido2;
-    }
-    public String getClienteDni() {
-        return this.clienteDni;
-    }
-    public String getClienteTelefono() {
-        return this.clienteTelefono;
-    }
-    public Date getClienteFecha() {
-        return this.clienteFecha;
-    }
-    public String getClienteUsuario() {
-        return this.clienteUsuario;
-    }
-    public String getClientePassword() {
-        return this.clientePassword;
-    }
-    public Usuarios getUsuarioCreado() {
-        return this.usuarioCreado;
-    }
-    
-    public void setClienteNombre(String nombre) {
-        this.clienteNombre = nombre;
-    }
-    public void setClienteApellido1(String apellido) {
-        this.clienteApellido1 = apellido;
-    }
-    public void setClienteApellido2(String apellido) {
-        this.clienteApellido2 = apellido;
-    }
-    public void setClienteDni(String dni) {
-        this.clienteDni = dni;
-    }
-    public void setClienteTelefono(String telefono) {
-        this.clienteTelefono = telefono;
-    }
-    public void setClienteFecha(Date fecha) {
-        this.clienteFecha = fecha;
-    }
-    public void setClienteUsuario(String user) {
-        this.clienteUsuario = user;
-    }
-    public void setClientePassword(String password) {
-        this.clientePassword = password;
-    }
+  
     public void setUsuarioCreado(Usuarios user) {
         this.usuarioCreado = user;
     }
-
-    public Usuarios getSelected() {
-        return selected;
-    }
-
-    public void setSelected(Usuarios selected) {
-        this.selected = selected;
-    }
   
-    protected void setEmbeddableKeys() {
-    }
-
-    protected void initializeEmbeddableKey() {
+    public Usuarios getUsuarioCreado() {
+        return this.usuarioCreado;
     }
 
     private UsuariosFacade getFacade() {
         return ejbFacade;
-    }
-
-    public Usuarios prepareCreate() {
-        selected = new Usuarios();
-        initializeEmbeddableKey();
-        return selected;
-    }
-
-    public void create() {
-        
-        persist(PersistAction.CREATE, ResourceBundle.getBundle("/Bundle").getString("UsuariosCreated"));
-        if (!JsfUtil.isValidationFailed()) {
-            items = null;    // Invalidate list of items to trigger re-query.
-        }
-    }
-
-    public void update() {
-        persist(PersistAction.UPDATE, ResourceBundle.getBundle("/Bundle").getString("UsuariosUpdated"));
-    }
-
-    public void destroy() {
-        persist(PersistAction.DELETE, ResourceBundle.getBundle("/Bundle").getString("UsuariosDeleted"));
-        if (!JsfUtil.isValidationFailed()) {
-            selected = null; // Remove selection
-            items = null;    // Invalidate list of items to trigger re-query.
-        }
     }
 
     public List<Usuarios> getItems() {
@@ -153,34 +80,6 @@ public class UsuariosController implements Serializable {
             items = getFacade().findAll();
         }
         return items;
-    }
-
-    private void persist(PersistAction persistAction, String successMessage) {
-        if (selected != null) {
-            setEmbeddableKeys();
-            try {
-                if (persistAction != PersistAction.DELETE) {
-                    getFacade().edit(selected);
-                } else {
-                    getFacade().remove(selected);
-                }
-                JsfUtil.addSuccessMessage(successMessage);
-            } catch (EJBException ex) {
-                String msg = "";
-                Throwable cause = ex.getCause();
-                if (cause != null) {
-                    msg = cause.getLocalizedMessage();
-                }
-                if (msg.length() > 0) {
-                    JsfUtil.addErrorMessage(msg);
-                } else {
-                    JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
-                }
-            } catch (Exception ex) {
-                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
-                JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
-            }
-        }
     }
 
     public Usuarios getUsuarios(java.lang.Integer id) {
@@ -253,48 +152,46 @@ public class UsuariosController implements Serializable {
     
     public void comprobarFecha(FacesContext context, UIComponent comp, Object value) {
         Date fecha = (Date) value;
+        Date fechaValida = new Date(104, 1, 1);
         
-        
-        
-        System.out.println(fecha);
-        System.out.println("La fecha no es correcta");
+        if(fechaValida.compareTo(fecha) < 0) {
+            ((UIInput) comp).setValid(false);
+            JsfUtil.addErrorMessage("Debe tener más de 18 años");
+        }
     }
     
-    public void crearCliente() throws IOException {
- 
-        Usuarios nuevoUsuario = new Usuarios();
-
-        //
+    public void crearUsuario() throws IOException {
         
-        nuevoUsuario.setNombre(clienteNombre);
-        nuevoUsuario.setApellido1(clienteApellido1);
-        nuevoUsuario.setApellido2(clienteApellido2);
-        nuevoUsuario.setDni(clienteDni);
-        nuevoUsuario.setTelefono(clienteTelefono);
-        nuevoUsuario.setFechaNacimiento(clienteFecha);
-        nuevoUsuario.setCreadoEn(new Date());
-        nuevoUsuario.setUltimaConexion(new Date());
-
-        // Creación Usuario
-        nuevoUsuario.setUsuario(clienteUsuario);
-        nuevoUsuario.setPassword(clientePassword);
-
-        //
-        getFacade().crearNuevoCliente(nuevoUsuario);
+        this.usuarioInfo.setCreadoEn(new Date());
+        this.usuarioInfo.setUltimaConexion(new Date());
+        
+        getFacade().create(this.usuarioInfo);
         JsfUtil.addSuccessMessage("Se ha creado el nuevo cliente");
 
-        this.usuarioCreado = nuevoUsuario;
+        this.usuarioCreado = this.usuarioInfo;
         
-        // Reset
+        this.items = null;
+        this.usuarioInfo = new Usuarios();
+        this.usuarioInfo.setIdRol(this.usuarioCreado.getIdRol());
+    }
+    
+    public void crearUsuarioAdmin() throws IOException {
         
-        clienteNombre = "";
-        clienteApellido1 = "";
-        clienteApellido2 = "";
-        clienteDni = "";
-        clienteTelefono = "";
-        clienteFecha = null;
-        clienteUsuario = "";
-        clientePassword = "";
+        this.usuarioInfo.setCreadoEn(new Date());
+        this.usuarioInfo.setUltimaConexion(new Date());
+        
+        getFacade().create(this.usuarioInfo);
+        JsfUtil.addSuccessMessage("Se ha creado un nuevo usuario");
+        
+        this.items = null;
+        this.usuarioInfo = null;
+    }
+    
+    public void editarUsuario() {
+        getFacade().edit(this.usuarioInfo);
+        JsfUtil.addSuccessMessage("Has editado al usuario");
+        
+        this.usuarioInfo = null;
     }
     
     public void borrarCliente(Usuarios usuario) throws IOException {
